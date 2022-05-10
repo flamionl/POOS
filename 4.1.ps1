850146bd94a8aafafac0346d663fbf0b4dc5bea6
flamionl <55742507+flamionl@users.noreply.github.com>
2022-05-10 17:13:49 +0200

    Create 4.1.ps1

---
+++ 4.1.ps1
@@ -0,0 +1,38 @@
+param(
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+    [string]$h
+)
+
+function portscanning($port) {
+    $socket = New-Object system.Net.Sockets.TcpClient
+    $conn = $socket.ConnectAsync($h,$port)
+    $msg = "Port $port "
+    #Waiting for connection to be established or refused
+    for($i=0; $i -lt 5; $i++){
+        if ($conn.IsCompleted) {
+            break
+        }
+        Start-Sleep -Milliseconds 50
+    }
+    $status = "Filtered"
+    if ($conn.IsFaulted -and $conn.Exception -match "refus") {
+        $status = "Closed"
+    } elseif ($conn.Status -eq "RanToCompletion") {
+        $status = "Open"
+    }
+    $socket.Close()
+    $msg += $status
+    Write-Host $msg
+
+
+}
+
+if (Test-Connection -Count 1 $h) {
+    for($port=1; $port -le 1024; $port++){
+        portscanning $port
+        
+    }
+}else {
+    Write-Host "Host unreachable"
+}
+
